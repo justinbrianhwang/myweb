d6ee5bb33b3972a7638e6fc4d71aea589ac1f460
justinbrianhwang <sunjun7559@gmail.com>
2026-04-18 11:54:30 +0900

    Add CS Deadlines embedded page with iframe and fallback

--- js/sidebar.js
+++ js/sidebar.js
@@ -44,6 +44,7 @@ function createSidebar() {
       <li><a href="https://www.sjhwangresearch.com" target="_blank" class="menu-item"><span class="menu-color color-research"></span>Research</a></li>
       <li><a href="${basePath}pages/skills.html" class="menu-item"><span class="menu-color color-skills"></span>Skills & Tools & Education</a></li>
       <li><a href="${basePath}pages/experience.html" class="menu-item"><span class="menu-color color-exp"></span>Experience</a></li>
+      <li><a href="${basePath}pages/deadlines.html" class="menu-item"><span class="menu-color color-skills"></span>CS Deadlines</a></li>
       <li class="has-submenu">
         <button class="menu-item submenu-toggle">
           <span><span class="menu-color color-contact"></span>Contact</span>
